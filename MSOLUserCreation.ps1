b2c113acf08714bb5e73cf5301022b314620c151
nabil2199 <nabil2199@gmail.com>
2017-04-03 11:35:54 +0200

    parameters and office365 session

--- MSOLUserCreation.ps1
+++ MSOLUserCreation.ps1
@@ -4,3 +4,10 @@ Verion 0.1
 OCWS
 BSD 3-Clause License
 #>
+
+param([string]$userCsv = "C:\Sources\users.csv")
+
+#Open Office365 session
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session 
+
