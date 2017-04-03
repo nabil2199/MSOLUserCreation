76b4d916065b3be1446489f745261135f0386bec
nabil2199 <nabil2199@gmail.com>
2017-04-03 11:40:12 +0200

    Logging

--- MSOLUserCreation.ps1
+++ MSOLUserCreation.ps1
@@ -7,7 +7,24 @@ BSD 3-Clause License
 
 param([string]$userCsv = "C:\Sources\users.csv")
 
-#Open Office365 session
+Import-Module msonline
+
+#Ouverture session Office365
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session 
+
+#Fonction Journalisation
+Function Write-Log {
+    Param ([string]$User="",[string]$Type="",[string]$Content)
+    $time = get-date -format "yyyy-MM-dd HH:mm:ss[ffffff]"
+    [string]$Path=$PSScriptRoot+"\ActivationOffice365-"+$date+".log"
+    if ($User -eq "") {
+        $Output = $time + ", " + $Type + " - " + $Content
+    }
+    else {
+        $Output = $time + ", " + $Type + " - " + $User + " : " + $Content
+    }
+   Add-content -encoding utf8 -Path $Logfile -value $Output -ErrorAction SilentlyContinue
+   write-host $Output
+}
 
